631a1d18ac0e5d07a531057e8ea5cc9c274e9d39
Robby Alamsyah S. P <105335505+roybstry@users.noreply.github.com>
2023-12-05 10:12:08 +0700

    prediksi_mobil.py

--- prediksi_mobil.py
+++ prediksi_mobil.py
@@ -5,11 +5,11 @@ model = pickle.load(open('prediksi_mobil.sav', 'rb'))
 
 st.title('Prediksi Harga Mobil Bekas')
 
-year = st.number_input('Input Tahun Mobil')
-mileage = st.number_input('Input Km Mobil')
-tax = st.number_input('Input Pajak Mobil')
-mpg = st.number_input('Input Konsumsi BBM Mobil')
-engineSize = st.number_input('Input Engine Size')
+year = st.number_input('Tahun Mobil')
+mileage = st.number_input('Km Mobil')
+tax = st.number_input('Pajak Mobil')
+mpg = st.number_input('Konsumsi BBM Mobil')
+engineSize = st.number_input('Engine Size')
 
 predict = ''
 
